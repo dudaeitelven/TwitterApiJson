d124dd1d74286d600a83b9e5bd298fda339215fd
Eduardo Eitelven <dudaeitelven@gmail.com>
2019-10-26 18:59:27 -0300

    ok

--- TwitterApiJson/src/principal/GerarIndiceHash.java
+++ TwitterApiJson/src/principal/GerarIndiceHash.java
@@ -12,23 +12,57 @@ public class GerarIndiceHash {
 	private static ArrayList<DadosTweet> aDadosTweet = new ArrayList<DadosTweet>();
 	
 	public void GeraIndiceHash() {
+		String keyToSearch = null;
+		String ValueToSearch = null;
+		long indice = 0;
+		
 		AlimentarIndiceHash();
 		
-        for(Map.Entry<String, List<DadosTweet>> entry: hash.entrySet()) { 
-        	System.out.println(hash.keySet());
-        	
-            for(DadosTweet c: entry.getValue()) {
-                System.out.println(c.getId_tweet() + " - " + c.getData().trim() + " - " + c.getLinha());
-            }
-        }	
-        
-        try {
-			BusarSeek.ProcurarNoArquivoSeek(9);
-		} catch (IOException e) {
-			e.printStackTrace();
+//	    for(Map.Entry<String, List<DadosTweet>> entry: hash.entrySet()) { 
+//    	System.out.println(hash.keySet());
+//    	
+//        for(DadosTweet c: entry.getValue()) {
+//            System.out.println(c.getId_tweet() + " - " + c.getData().trim() + " - " + c.getLinha());
+//        }
+//    }	
+		      
+		keyToSearch = "2019-10-24";
+		ValueToSearch = "1187503296241643520";
+		       
+		indice = BuscarIndiceHash(keyToSearch,ValueToSearch);
+		
+		if (indice != 0) {
+			 try {
+				BusarSeek.ProcurarNoArquivoSeek(indice);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
+		else {
+			System.out.println("Valor " + ValueToSearch + " nao encontrado no indice hash! \n");
+		}
+       
 	}
 	
+	public static long BuscarIndiceHash (String keyToSearch, String ValueToSearch) {
+		long linha = 0;
+		String ValueHash = null;
+		
+        for (String key : hash.keySet()) {
+            List<DadosTweet> value = hash.get(key);
+            
+            for (int i=0; i < value.size();i++) {
+            	ValueHash = value.get(i).getId_tweet().trim();
+            	if (ValueHash.equals(ValueToSearch)) {
+            		return value.get(i).getLinha();
+            	}
+            }
+        }
+		
+		return 0;
+	}
+	
+
 	public static void AlimentarIndiceHash () {
 		String keyToSearch = null;
 
